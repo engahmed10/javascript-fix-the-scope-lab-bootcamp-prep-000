31e5600542470b6b42c15f5573ae0b6f03562526
AHMED AFANDY <eng.computer1@yahoo.com>
2020-02-13 00:10:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var animal = 'dog'
+const animal = 'dog'
 
 function myAnimal() {
   // You should not need to modify this function
@@ -11,7 +11,7 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: Hard-coding 'cat' below will not work
-  const animal ='monkey'
+   animal ='monkey'
   return animal
   
 }
